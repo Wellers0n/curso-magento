34dc3e3f5f6a395c94fd5f2538aef40154c69e0c
Wellerson <Wellers0n_@hotmail.com>
2018-06-22 15:16:50 -0300

    primeiro commit

---
+++ js/configuravel.js
@@ -0,0 +1,5 @@
+Product.Config.prototype.configureElement = Product.Config.prototype.configureElement.wrap(function(parentMethod, element){
+console.log("passou primeiro");
+return parentMethod(element);
+});
+
